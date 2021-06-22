88ffdf41d02a246d944dcf3ec81a245eddfb3161
Kurt Lourens <hi@kurtlourens.com>
2021-06-22 16:50:40 +0200

    register plugins

--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -4,9 +4,12 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <platform_device_id_windows/platform_device_id_windows_plugin.h>
 #include <url_launcher_windows/url_launcher_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  PlatformDeviceIdWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("PlatformDeviceIdWindowsPlugin"));
   UrlLauncherPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherPlugin"));
 }
