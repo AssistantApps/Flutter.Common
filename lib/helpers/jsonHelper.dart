8acab02a17d8361b9b9562bda3304d1415f18b1f
Kurt Lourens <kurt.lourens@outlook.com>
2021-08-28 21:12:07 +0200

    make json helpers more robust

--- lib/helpers/jsonHelper.dart
+++ lib/helpers/jsonHelper.dart
@@ -1,23 +1,31 @@
 import 'package:intl/intl.dart';
 
-String readStringSafe(Map<String, dynamic> json, String prop) =>
-    (json[prop] == null) ? null : json[prop];
+String readStringSafe(Map<String, dynamic> json, String prop) {
+  if (json == null) return '';
+  if (json[prop] == null) return '';
+  return json[prop];
+}
 
-bool readBoolSafe(Map<dynamic, dynamic> json, String prop) =>
-    (json[prop] == null) ? false : json[prop] as bool;
+bool readBoolSafe(Map<dynamic, dynamic> json, String prop) {
+  if (json == null) return false;
+  if (json[prop] == null) return false;
+  return json[prop] as bool;
+}
 
 int readIntSafe(Map<dynamic, dynamic> json, String prop) {
   if (json == null) return 0;
   dynamic value = json[prop];
   if (value is int) return value;
-  return (value == null) ? 0 : int.tryParse(json[prop]);
+  if (value == null) return 0;
+  return int.tryParse(json[prop]);
 }
 
 double readDoubleSafe(Map<dynamic, dynamic> json, String prop) {
   if (json == null) return 0.0;
   dynamic value = json[prop];
   if (value is double) return value;
-  return (value == null) ? 0.0 : double.tryParse(json[prop]);
+  if (value == null) return 0.0;
+  return double.tryParse(json[prop]);
 }
 
 DateTime readDateSafe(Map<dynamic, dynamic> json, String prop) {
@@ -32,6 +40,7 @@ DateTime readDateSafe(Map<dynamic, dynamic> json, String prop) {
 
 List<T> readListSafe<T>(
     Map<dynamic, dynamic> json, String prop, T Function(dynamic) mapper) {
+  if (json == null) return List.empty(growable: true);
   if (json[prop] == null) return List.empty(growable: true);
   return (json[prop] as List).map((item) => mapper(item)).toList();
 }
