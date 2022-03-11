1d94b2d8e0557e3ec73e036ff7d630f4acc9faf1
Kurt Lourens <hi@kurtlourens.com>
2022-03-11 11:25:00 +0100

    update new banner

--- lib/components/common/newBanner.dart
+++ lib/components/common/newBanner.dart
@@ -3,11 +3,12 @@ import 'package:flutter/material.dart';
 import '../../contracts/enum/localeKey.dart';
 import '../../integration/dependencyInjection.dart';
 
-Widget wrapInNewBanner(BuildContext context, LocaleKey message, Widget child) =>
+Widget wrapInNewBanner(BuildContext context, LocaleKey message, Widget child,
+        {BannerLocation location = BannerLocation.topEnd}) =>
     ClipRect(
       child: Banner(
         message: getTranslations().fromKey(message),
-        location: BannerLocation.topEnd,
+        location: location,
         child: child,
       ),
     );
