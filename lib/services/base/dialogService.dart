347ff718d8c47c47c7a79a201d02554e59a4595b
Kurt Lourens <kurt.lourens@outlook.com>
2022-05-14 23:10:18 +0200

    0.1.48

--- lib/services/base/dialogService.dart
+++ lib/services/base/dialogService.dart
@@ -6,23 +6,69 @@ import 'interface/IDialogService.dart';
 
 class DialogService implements IDialogService {
   //
-  void showSimpleDialog(context, String title, Widget content,
-      {List<Widget>? buttons}) {}
-  void showSimpleHelpDialog(context, String title, String helpContent,
-      {List<Widget>? buttons}) {}
-  Widget simpleDialogCloseButton(context, {Function()? onTap}) => Container();
-  Widget simpleDialogPositiveButton(context,
-          {LocaleKey? title, Function? onTap}) =>
+  @override
+  void showSimpleDialog(
+    context,
+    String title,
+    Widget content, {
+    List<Widget> Function(BuildContext)? buttonBuilder,
+  }) {}
+
+  @override
+  void showSimpleHelpDialog(
+    context,
+    String title,
+    String helpContent, {
+    List<Widget> Function(BuildContext)? buttonBuilder,
+  }) {}
+
+  @override
+  Widget simpleDialogCloseButton(
+    context, {
+    Function()? onTap,
+  }) =>
       Container();
-  void showOptionsDialog(context, String title, List<DropdownOption> options,
-      {String selectedValue = '', Function(String value)? onSuccess}) {}
-  void showQuantityDialog(context, TextEditingController controller,
-      {String? title, Function(String)? onSuccess}) {}
-  void showStarDialog(context, String title,
-      {int currentRating = 0, Function(int)? onSuccess}) {}
-  Future<String> asyncInputDialog(BuildContext context, String title,
-          {String? defaultText,
-          List<Widget>? actions,
-          TextInputType? inputType}) async =>
+
+  @override
+  Widget simpleDialogPositiveButton(
+    context, {
+    LocaleKey? title,
+    Function? onTap,
+  }) =>
+      Container();
+
+  @override
+  void showOptionsDialog(
+    context,
+    String title,
+    List<DropdownOption> options, {
+    String selectedValue = '',
+    Function(BuildContext ctx, String value)? onSuccess,
+  }) {}
+
+  @override
+  void showQuantityDialog(
+    context,
+    TextEditingController controller, {
+    String? title,
+    Function(BuildContext ctx, String)? onSuccess,
+  }) {}
+
+  @override
+  void showStarDialog(
+    context,
+    String title, {
+    int currentRating = 0,
+    Function(BuildContext ctx, int)? onSuccess,
+  }) {}
+
+  @override
+  Future<String> asyncInputDialog(
+    BuildContext context,
+    String title, {
+    String? defaultText,
+    List<Widget> Function(BuildContext)? buttonBuilder,
+    TextInputType? inputType,
+  }) async =>
       '';
 }
--- lib/services/base/interface/IDialogService.dart
+++ lib/services/base/interface/IDialogService.dart
@@ -5,23 +5,61 @@ import '../../../contracts/search/dropdownOption.dart';
 
 class IDialogService {
   //
-  void showSimpleDialog(context, String title, Widget content,
-      {List<Widget>? buttons}) {}
-  void showSimpleHelpDialog(context, String title, String helpContent,
-      {List<Widget>? buttons}) {}
-  Widget simpleDialogCloseButton(context, {Function()? onTap}) => Container();
-  Widget simpleDialogPositiveButton(context,
-          {LocaleKey? title, Function? onTap}) =>
+  void showSimpleDialog(
+    context,
+    String title,
+    Widget content, {
+    List<Widget> Function(BuildContext)? buttonBuilder,
+  }) {}
+
+  void showSimpleHelpDialog(
+    context,
+    String title,
+    String helpContent, {
+    List<Widget> Function(BuildContext)? buttonBuilder,
+  }) {}
+
+  Widget simpleDialogCloseButton(
+    context, {
+    Function()? onTap,
+  }) =>
       Container();
-  void showOptionsDialog(context, String title, List<DropdownOption> options,
-      {String selectedValue = '', Function(String value)? onSuccess}) {}
-  void showQuantityDialog(context, TextEditingController controller,
-      {String? title, Function(String)? onSuccess}) {}
-  void showStarDialog(context, String title,
-      {int currentRating = 0, Function(int)? onSuccess}) {}
-  Future<String> asyncInputDialog(BuildContext context, String title,
-          {String? defaultText,
-          List<Widget>? actions,
-          TextInputType? inputType}) async =>
+
+  Widget simpleDialogPositiveButton(
+    context, {
+    LocaleKey? title,
+    Function? onTap,
+  }) =>
+      Container();
+
+  void showOptionsDialog(
+    context,
+    String title,
+    List<DropdownOption> options, {
+    String selectedValue = '',
+    Function(BuildContext ctx, String value)? onSuccess,
+  }) {}
+
+  void showQuantityDialog(
+    context,
+    TextEditingController controller, {
+    String? title,
+    Function(BuildContext ctx, String)? onSuccess,
+  }) {}
+
+  void showStarDialog(
+    context,
+    String title, {
+    int currentRating = 0,
+    Function(BuildContext ctx, int)? onSuccess,
+  }) {}
+
+  Future<String> asyncInputDialog(
+    BuildContext context,
+    String title, {
+    String? defaultText,
+    List<Widget> Function(BuildContext)? buttonBuilder,
+    TextInputType? inputType,
+  }) async =>
       '';
 }
