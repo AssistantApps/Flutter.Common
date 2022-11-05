ee45f044303b247becba6e2fc67560f451be467a
Kurt Lourens <kurt.lourens@outlook.com>
2022-11-05 11:19:42 +0100

    🔖 0.2.0

--- lib/components/adaptive/bottomModal.dart
+++ lib/components/adaptive/bottomModal.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/material.dart';
-import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+import 'package:flutter/material.dart' hide ModalBottomSheetRoute;
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart'
+    hide ModalBottomSheetRoute;
 
 // import '../../helpers/deviceHelper.dart';
 
