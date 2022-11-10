4c60a7c964fc3116ce18af5bb4f03a145ab33610
Kurt Lourens <kurt.lourens@outlook.com>
2022-11-11 00:00:23 +0100

    🔖 0.2.4

--- lib/components/common/badge.dart
+++ lib/components/common/badge.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide Badge;
 import 'package:badges/badges.dart';
 
 import '../../integration/dependencyInjection.dart';
