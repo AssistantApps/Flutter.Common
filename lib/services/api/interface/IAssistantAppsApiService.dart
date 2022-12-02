d609fa00f99c0158c44dd765457fa0491062e4b1
Kurt Lourens <hi@kurtlourens.com>
2022-12-02 19:00:10 +0100

    🔖 0.2.15

--- lib/services/api/interface/IAssistantAppsApiService.dart
+++ lib/services/api/interface/IAssistantAppsApiService.dart
@@ -1,11 +1,19 @@
+import '../../../contracts/generated/appNoticeViewModel.dart';
 import '../../../contracts/generated/translatorLeaderboardItemViewModel.dart';
 import '../../../contracts/results/paginationResultWithValue.dart';
+import '../../../contracts/results/resultWithValue.dart';
 
 class IAssistantAppsApiService {
   //
   Future<PaginationResultWithValue<List<TranslatorLeaderboardItemViewModel>>>
       getTranslators() async {
     return PaginationResultWithValue<List<TranslatorLeaderboardItemViewModel>>(
+        false, List.empty(growable: true), 1, 0, '');
+  }
+
+  Future<ResultWithValue<List<AppNoticeViewModel>>> getAppNotices(
+      String langCode) async {
+    return PaginationResultWithValue<List<AppNoticeViewModel>>(
         false, List.empty(growable: true), 1, 0, '');
   }
 }
