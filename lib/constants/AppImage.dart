a425705f0e9aaa7747b21604aefbd8b550e99499
Kurt Lourens <kurt.lourens@outlook.com>
2023-01-17 19:57:58 +0100

    🎨 Clean up code

--- lib/constants/AppImage.dart
+++ lib/constants/AppImage.dart
@@ -2,49 +2,47 @@ class AppImage {
   static const String base = 'assets/images/';
   static const String guide = 'guide/';
   static const String drawer = 'drawer/';
-  static const String infographic = 'infographic/';
-  static const String donationsFolder = 'donations/';
+  static const String infographic = '${base}infographic/';
+  static const String donationsFolder = '${base}donations/';
 
-  static const String donApplePay = base + donationsFolder + 'applePay.png';
-  static const String donBat = base + donationsFolder + 'bat.png';
-  static const String donBuyMeACoffee = base + donationsFolder + 'bmc.png';
-  static const String donGooglePay = base + donationsFolder + 'googlePay.png';
-  static const String donKofi = base + donationsFolder + 'kofi.png';
-  static const String donOpenCollective = base + donationsFolder + 'openCo.png';
-  static const String donPatreon = base + donationsFolder + 'patreon.png';
-  static const String donPatreonAlt =
-      base + donationsFolder + 'patreonWhite.png';
-  static const String donPayPal = base + donationsFolder + 'payPal.png';
-  static const String donUnknown =
-      base + donationsFolder + 'unknownDonation.png';
+  static const String donApplePay = '${donationsFolder}applePay.png';
+  static const String donBat = '${donationsFolder}bat.png';
+  static const String donBuyMeACoffee = '${donationsFolder}bmc.png';
+  static const String donGooglePay = '${donationsFolder}googlePay.png';
+  static const String donKofi = '${donationsFolder}kofi.png';
+  static const String donOpenCollective = '${donationsFolder}openCo.png';
+  static const String donPatreon = '${donationsFolder}patreon.png';
+  static const String donPatreonAlt = '${donationsFolder}patreonWhite.png';
+  static const String donPayPal = '${donationsFolder}payPal.png';
+  static const String donUnknown = '${donationsFolder}unknownDonation.png';
 
-  static const String error1 = base + infographic + 'error1.png';
-  static const String successGuide = base + infographic + 'successGuide.png';
-  static const String successGuideSVG = base + infographic + 'successGuide.svg';
-  static const String authSVG = base + infographic + 'authentication.svg';
-  static const String defaultGuide = base + guide + 'noImage.png';
+  static const String error1 = '${infographic}error1.png';
+  static const String successGuide = '${infographic}successGuide.png';
+  static const String successGuideSVG = '${infographic}successGuide.svg';
+  static const String authSVG = '${infographic}authentication.svg';
+  static const String defaultGuide = '$base${guide}noImage.png';
 
   static const String assistantApps = 'assistantApps.png';
   static const String unknown = 'unknown.png';
 
-  static const String error = base + 'error.png';
-  static const String snowflake = base + 'special/snowflake.png';
-  static const String heart = base + 'special/heart.png';
-  static const String specialPatreonLogo = base + 'special/patreonTile.png';
+  static const String error = '${base}error.png';
+  static const String snowflake = '${base}special/snowflake.png';
+  static const String heart = '${base}special/heart.png';
+  static const String specialPatreonLogo = '${base}special/patreonTile.png';
 
   // Team
   static const String baseTeamPictureUrl =
       'https://cdn.assistantapps.com/team/';
   static const String teamPictureUrlKhaozTopsy =
-      baseTeamPictureUrl + 'KhaozTopsy.png';
-  static const String teamPictureUrlRobo = baseTeamPictureUrl + 'Robo.png';
+      '${baseTeamPictureUrl}KhaozTopsy.png';
+  static const String teamPictureUrlRobo = '${baseTeamPictureUrl}Robo.png';
   static const String teamPictureUrlLilo =
-      baseTeamPictureUrl + 'LionessLilo.jpg';
+      '${baseTeamPictureUrl}LionessLilo.jpg';
   static const String teamPictureUrlSphynxcolt =
-      baseTeamPictureUrl + 'Sphynxcolt.png';
-  static const String teamPictureUrlLenni = baseTeamPictureUrl + 'lenni.png';
+      '${baseTeamPictureUrl}Sphynxcolt.png';
+  static const String teamPictureUrlLenni = '${baseTeamPictureUrl}lenni.png';
   static const String teamPictureUrlCyberpunk =
-      baseTeamPictureUrl + 'cyberpunk2350.png';
+      '${baseTeamPictureUrl}cyberpunk2350.png';
 
   static const String onlinePatreonIcon =
       'https://scrapassistant.com/assets/assets/img/drawer/patreon.png';
