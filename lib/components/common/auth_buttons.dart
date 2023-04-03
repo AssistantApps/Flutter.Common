35a2238562670adb162790900f0824f7dc855a1c
Kurt Lourens <kurt.lourens@outlook.com>
2023-04-03 22:00:21 +0200

    🔖 0.2.34

--- lib/components/common/auth_buttons.dart
+++ lib/components/common/auth_buttons.dart
@@ -1,13 +1,12 @@
 import 'patreon_auth_button.dart';
 import 'package:flutter/material.dart';
-import 'package:auth_buttons/auth_buttons.dart' show GoogleAuthButton;
 
 class AuthButton {
-  static Widget google({required void Function() onPressed}) =>
-      GoogleAuthButton(
-        onPressed: onPressed,
-        themeMode: ThemeMode.dark,
-      );
+  // static Widget google({required void Function() onPressed}) =>
+  //     GoogleAuthButton(
+  //       onPressed: onPressed,
+  //       themeMode: ThemeMode.dark,
+  //     );
 
   static Widget patreon({required void Function() onPressed}) =>
       PatreonAuthButton(onPressed: onPressed);
